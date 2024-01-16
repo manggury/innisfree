bcb4c8d09950ff99d1c897a6b635818335eac07d
manggury <148739312+manggury@users.noreply.github.com>
2024-01-16 18:22:21 +0900

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+$(function () {
+    $('.main_visual .visual_slide').slick({
+        arrows: false,
+    });
+    $('.main_content .content_wrap .content_slide').slick({
+        arrows: false,
+    });
+});
