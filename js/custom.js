e4d8c6fc8a1ea0b884d4bb2ab80f56897cbd6288
manggury <148739312+manggury@users.noreply.github.com>
2024-01-17 17:45:03 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,12 +1,30 @@
 $(function () {
+    // $(window).on('scroll', function () {
+    //     const scrollHeight = $(window).scrollTop();
+    //     console.log(scrollHeight);
+    //     if (scrollHeight > 0) {
+    //         $('.header').addClass('on')
+    //     } else {
+    //         $('.header').removeClass('on')
+    //     }
+    // })
     $('.main_visual .visual_slide').slick({
         arrows: false,
     });
+
     $('.main_content .content_wrap .content_slide').slick({
         arrows: false,
     });
+    $('.content_wrap .dot_area>ul li').click(function () {
+        var slideIndex = $(this);
+        $('.main_content .content_wrap .content_slide').slick('slickGoTo', slideIndex);
+    });
+
     $('.main_promotion .promotion_wrap .right .promotion_slide').slick({
         arrows: false,
-        silidToShow: 4,
+        slidesToShow: 4,
+    });
+    $('.promotion_wrap .left .txt .arrows .left_arrow').click(function () {
+        $('.main_promotion .promotion_wrap .right .promotion_slide').slickprev();
     });
 });
