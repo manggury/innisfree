aceda1ad4cc7054ac9b1f51fb8d36b7b511aaa72
manggury <148739312+manggury@users.noreply.github.com>
2024-01-18 18:07:55 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,27 @@
 $(function () {
 
+    $(window).on('scroll', function () {
+        const scrollHeight = $(window).scrollTop();
+        console.log(scrollHeight);
+        if (scrollHeight > 0) {
+            $('.header_bottom').addClass('on')
+        } else {
+            $('.header_bottom').removeClass('on')
+        }
+    })
+
+    $('.header_bottom .menu_btn span').on('click', function (e) {
+        e.preventDefault();
+        $('.header_bottom .gnb').toggleClass('on');
+    });
+
     $('.main_visual .visual_slide').slick({
         arrows: true,
         slidesToShow: 1,
         prevArrow: $('.main_visual .left_arrow'),
         nextArrow: $('.main_visual .right_arrow'),
     });
+
 
     $('.main_content .content_wrap .content_slide').slick({
         arrows: false,
